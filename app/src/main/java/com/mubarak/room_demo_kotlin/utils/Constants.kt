8f78b1782b05788fdf0b6345c8de7ee5673679bc
mubarak_ansari <mubarak.ansari@brainvire.com>
2022-07-24 14:35:05 +0530

    update

--- app/src/main/java/com/mubarak/room_demo_kotlin/utils/Constants.kt
+++ app/src/main/java/com/mubarak/room_demo_kotlin/utils/Constants.kt
@@ -1,12 +1,5 @@
 package com.mubarak.room_demo_kotlin.utils
 
 object Constants {
-    const val LOG_IN_KEY = "loginkey"
-    const val MYSHAREDPREF = "om.example.iifl_assignment.utils.mysharedpref"
     const val BASE_URL = "https://jsonplaceholder.typicode.com/"
-    const val AUTHORIZATIONKEY = "Basic aW5kaWFpbmZvbGluZVw1cGFuZHJvaWQ6MnhxUzV2L1g5Og=="
-    const val CONTENTTYPE = "application/json"
-    const val SORTKEY = "SORTKEY"
-    const val RISKOMETERVALUE = "riskometervalue"
-    const val RECOMMENDFUNDFLAG = "RecommenedFundFlag"
 }
